bdf81bcbd8623d5d4492c528e34743e0e7940f36
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-09-10 10:57:55 -0300

    Change calendar table to grid and add months menu

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,26 +7,56 @@ import '../styles/Calendar.css';
 function Calendar() {
   Settings.defaultLocale = 'en-US';
   const [dateObj, setDateObj] = useState(DateTime.now());
+  const [showMonths, setShowMonths] = useState(false);
 
-  const changeMonth = {
-    next: () => {
-      setDateObj(dateObj.plus({ month: 1 }));
-    },
-    previous: () => {
-      setDateObj(dateObj.minus({ month: 1 }));
-    },
+  const renderMonths = () => {
+    const NUMBER_OF_MONTHS = 12;
+    const startDate = dateObj.startOf('year');
+    const dates = [startDate];
+    for (let i = 1; i < NUMBER_OF_MONTHS; i++) {
+      dates.push(startDate.plus({ month: i }));
+    }
+
+    return (
+      <div className='calendar-months'>
+        {dates.map((el) => (
+          <button
+            className='calendar-months__button'
+            type='button'
+            key={el.monthLong}
+            onClick={() => {
+              setDateObj(el);
+              setShowMonths(false);
+            }}
+          >
+            {el.monthLong}
+          </button>
+        ))}
+      </div>
+    );
   };
 
   return (
     <div className='calendar'>
       <div className='calendar__header'>
-        <input type='button' value='Previous' onClick={changeMonth.previous} />
-        <span>
-          {dateObj.monthLong} {dateObj.year}
-        </span>
-        <input type='button' value='Next' onClick={changeMonth.next} />
+        <input
+          type='button'
+          value='Previous'
+          onClick={() => setDateObj(dateObj.minus({ month: 1 }))}
+        />
+        <div>
+          <button type='button' onClick={() => setShowMonths(!showMonths)}>
+            {dateObj.monthLong}
+          </button>
+          <span>{dateObj.year}</span>
+        </div>
+        <input
+          type='button'
+          value='Next'
+          onClick={() => setDateObj(dateObj.plus({ month: 1 }))}
+        />
       </div>
-      <CalendarTable dateObj={dateObj} />
+      {showMonths ? renderMonths() : <CalendarTable dateObj={dateObj} />}
     </div>
   );
 }
--- src/components/CalendarTable.jsx
+++ src/components/CalendarTable.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 function CalendarTable({ dateObj }) {
+  const NUMBER_OF_DATES = 42;
+  const NUMBER_OF_WEEKDAYS = 7;
   const startDate = dateObj.startOf('month').startOf('week').minus({ day: 1 });
   const dates = [startDate];
-  for (let i = 1; i < 42; i++) {
+  for (let i = 1; i < NUMBER_OF_DATES; i++) {
     dates.push(startDate.plus({ day: i }));
   }
 
-  const renderWeekdays = () => {
-    const weekdays = dates.filter((_el, i) => i < 7).map((el) => el.weekdayShort);
-    return weekdays.map((el) => (
-      <div className='calendar-table__cell' key={el}>
-        {el}
-      </div>
-    ));
+  const handleClick = (obj) => {
+    console.log(obj);
   };
 
-  const renderDates = (row) => {
-    const arr = dates.filter((_el, i) => i >= row * 7 && i <= row * 7 + 6);
-    return arr.map((el, i) => (
-      <div className='calendar-table__cell' key={i}>
+  const renderWeekdays = () => {
+    return dates
+      .filter((_el, i) => i < NUMBER_OF_WEEKDAYS)
+      .map((el) => (
+        <div key={el.weekdayShort}>
+          {el.weekdayShort}
+        </div>
+      ));
+  };
+
+  const renderDates = () => {
+    return dates.map((el) => (
+      <div
+        className={cx('calendar-table__date', {
+          'text-light': el.month !== dateObj.month,
+        })}
+        key={el.ordinal}
+        onClick={() => handleClick(el)}
+      >
         {el.day}
       </div>
     ));
@@ -28,17 +41,8 @@ function CalendarTable({ dateObj }) {
 
   return (
     <div className='calendar-table'>
-      <div className='calendar-table__header'>
-        <div className='calendar-table__row'>{renderWeekdays()}</div>
-      </div>
-      <div className='calendar-table__body'>
-        <div className='calendar-table__row'>{renderDates(0)}</div>
-        <div className='calendar-table__row'>{renderDates(1)}</div>
-        <div className='calendar-table__row'>{renderDates(2)}</div>
-        <div className='calendar-table__row'>{renderDates(3)}</div>
-        <div className='calendar-table__row'>{renderDates(4)}</div>
-        <div className='calendar-table__row'>{renderDates(5)}</div>
-      </div>
+      {renderWeekdays()}
+      {renderDates()}
     </div>
   );
 }
